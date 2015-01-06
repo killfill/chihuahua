b784da6411a9fcbe9f453b37ed885e43413d5026
Phillip Neumann <pneumann@gmail.com>
2015-01-06 15:38:48 -0300

    send start/stop/reboot actions to vms

--- src/js/actions/app-bar.js
+++ src/js/actions/app-bar.js
@@ -5,7 +5,16 @@ module.exports = {
     trigger: function(context, actionName, id) {
 
         return function(e, selectedIdx, item) {
-            console.log('app-bar action:', context, actionName, id, item)
+
+            switch (context) {
+                case 'machine':
+                    handleMachine(actionName, id, item)
+                    break;
+
+                default:
+                    console.info('unhandled app-bar action', context, actionName, id, item)
+            }
+
             D.handleViewAction({
                 actionType: 'APP_BAR',
                 context: context,
@@ -15,5 +24,23 @@ module.exports = {
             })
 
         }
+    }
+}
+
+function handleMachine(actionName, id, item) {
+
+    var action = actionName === 'menu' ? item.payload : actionName
+
+    switch (action) {
+        case 'start':
+        case 'stop':
+        case 'reboot':
+            fifo.send('vms/' + id).put({body: {action: action}}, function(err, res, body) {
+                console.log('---->', err, res.statusCode, body)
+            })
+            break;
+
+        default:
+            console.info('Unhandled VM action', actionName, id, item)
     }
 }
